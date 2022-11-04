4ba7f98d565e74fbbcea6d0443bd64ef6409c195
kusham <kusham1998@gmail.com>
2022-11-04 22:52:52 +0530

    changes for app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import './App.css';
+import { useState } from "react";
+
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
+import "./App.css";
+import Auth from "./Pages/Auth/Auth";
+import Home from "./Pages/Home/Home";
 
 function App() {
+  // const [user, setUser] = useState(sessionStorage.getItem("profile"))
+
+  const user = useSelector((state) => state.authReducers.authData);
   return (
     <div className="App">
-      
+      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route
+            path="/"
+            element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
+          /> */}
+          <Route
+            path="/home"
+            element={user ? <Home /> : <Navigate to="../auth" />}
+          />
+          <Route
+            path="/auth"
+            element={user ? <Navigate to="../home" /> : <Auth />}
+          />
+          {/* <Route
+            path="/profile/:id"
+            element={user ? <Profile /> : <Navigate to="../auth" />}
+          />
+          <Route
+            path="/chat"
+            element={user ? <Chat /> : <Navigate to="../auth" />}
+          />
+          <Route path="*" element={<PageNotFound />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
