5a995862aac7459b9a48914af0e1b3c3656b6011
kusham <kusham1998@gmail.com>
2022-11-07 23:21:09 +0530

    create server file

---
+++ servers/server-3/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-Parser");
+const dbConnection = require("./Config/DBConnect");
+const cors = require("cors");
+
+const postRouts = require("./PostRoutes");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+dbConnection();
+
+const PORT = process.env.PORT || 5002;
+
+app.use("/post", postRouts);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+  
