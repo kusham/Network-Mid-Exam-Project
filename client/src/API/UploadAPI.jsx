a8524b9b8f6c284cf68289f549570c46c81412a4
kusham <kusham1998@gmail.com>
2022-11-15 20:31:28 +0530

    change base url of upload api

--- client/src/API/UploadAPI.jsx
+++ client/src/API/UploadAPI.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "" });
+const API = axios.create({ baseURL: "http://social-load-balancer-595777080.ap-south-1.elb.amazonaws.com" });
 
 export const uploadImage = (data) => API.post("/image/upload", data);
