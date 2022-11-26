bc14793e82823738d4528ad3015619d4b58344d3
kusham <kusham1998@gmail.com>
2022-11-26 22:51:22 +0530

    comment code set

--- client/src/API/AuthAPI.jsx
+++ client/src/API/AuthAPI.jsx
@@ -8,10 +8,10 @@ export const logIn= (formData)=> API.post('/auth/login',formData);
 export const signUp = (formData) => API.post('/auth/register', formData);
 
 
-export const getUser = (userId) => API.get(`/auth/user/${userId}`);
-export const updateUser = (id, formData) => API.put(`/auth/user/${id}`, formData);
-export const getAllUser = ()=> API.get('/auth/users/all')
-export const followUser = (id,data)=> API.put(`/auth/user/${id}/follow`, data)
-export const unFollowUser = (id, data)=> API.put(`/auth/user/${id}/unfollow`, data)
+// export const getUser = (userId) => API.get(`/auth/user/${userId}`);
+// export const updateUser = (id, formData) => API.put(`/auth/user/${id}`, formData);
+// export const getAllUser = ()=> API.get('/auth/users/all')
+// export const followUser = (id,data)=> API.put(`/auth/user/${id}/follow`, data)
+// export const unFollowUser = (id, data)=> API.put(`/auth/user/${id}/unfollow`, data)
 
 
