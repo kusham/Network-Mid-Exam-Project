093c074726bb110a16d2d98f92d3952faa0b58aa
kusham <kusham1998@gmail.com>
2022-11-27 23:06:05 +0530

    change base url to localhost

--- client/src/API/ChatAPI.jsx
+++ client/src/API/ChatAPI.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://social-load-balancer-595777080.ap-south-1.elb.amazonaws.com' });
+const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 export const createChat = (data) => API.post('/chat/', data);
 
