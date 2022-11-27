81ab7002bbd28b10e5dafc436b3b11e6b6718fc5
kusham <kusham1998@gmail.com>
2022-11-27 23:06:13 +0530

    change base url to localhost

--- client/src/API/UploadAPI.jsx
+++ client/src/API/UploadAPI.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://social-load-balancer-595777080.ap-south-1.elb.amazonaws.com" });
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
 export const uploadImage = (data) => API.post("/image/upload", data);
