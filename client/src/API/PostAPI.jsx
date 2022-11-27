aa4a28ca75a3e8a9c1aae45469ac35a90b5f23ee
kusham <kusham1998@gmail.com>
2022-11-27 23:06:09 +0530

    change base url to localhost

--- client/src/API/PostAPI.jsx
+++ client/src/API/PostAPI.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://social-load-balancer-595777080.ap-south-1.elb.amazonaws.com" });
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
 API.interceptors.request.use((req) => {
     if (sessionStorage.getItem('profile')) {
